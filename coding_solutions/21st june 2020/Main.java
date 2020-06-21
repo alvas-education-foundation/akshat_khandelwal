9f4a1d0bd31901d018b45b904dee63100e9a4c14
akshatkhandelwal1 <you@example.com>
2020-06-21 07:48:38 -0700

    21/06/2020

---
+++ coding_solutions/21st june 2020/Main.java
@@ -0,0 +1,29 @@
+//Write a Java Program to find area of Square, Rectangle and Circle using Method Overloading
+
+class Overload
+{
+    void area(float x)
+    {
+        System.out.println("the area of the square is "+Math.pow(x, 2)+" sq units");
+    }
+    void area(float x, float y)
+    {
+        System.out.println("the area of the rectangle is "+x*y+" sq units");
+    }
+    void area(double x)
+    {
+        double z = 3.14 * x * x;
+        System.out.println("the area of the circle is "+z+" sq units");
+    }
+}
+public class Main  
+{
+     public static void main(String args[]) 
+	{
+	   Overload ob = new Overload();
+	   ob.area(5);
+	   ob.area(11,12);
+	   ob.area(2.5);
+        }
+}
+
